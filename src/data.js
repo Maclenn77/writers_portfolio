10a460eb23bfa655bd6a5fb011b3c72f7a4ff100
Maclenn77 <maclenn77@gmail.com>
2022-02-22 01:26:13 -0600

    First version

---
+++ src/data.js
@@ -0,0 +1,47 @@
+export const projects = [
+    {
+        title: 'NewTechMag.net',
+        image: '',
+        url: 'http://newtechmag.net/',
+        language: ['English', 'Spanish'],
+        topic: ['Start-ups', 'Investments', 'Crypto', 'Gadgets'],
+        description: 'A blog about new technologies, with a special emphasis on innovative start-ups in developing countries.',
+        sample: 'http://newtechmag.net/2021/02/03/data-breaches-in-mexico-raises-concerns-about-biometric-requirements/'
+    },
+    {
+        title: 'Latin America Tech',
+        image: '',
+        url: 'http://www.latinamerica.tech/',
+        language: ['English', 'Spanish'],
+        topic: ['Start-ups', 'Latin America', 'Technology', 'Outsourcing'],
+        description: 'A blog that highlights innovation and digitization in the Latin American region, covering government efforts, entrepreneurship initiatives, and market trends of LatAm. ',
+        sample: 'http://www.latinamerica.tech/2021/10/24/for-latam-startups-finding-vc-funding-is-easier-than-ever/',
+    },
+    {
+        title: 'Executives.technology',
+        image: '',
+        url: 'https://executives.technology/',
+        language: ['English', 'Spanish'],
+        topic: ['Start-ups', 'Leadership', 'Biographies', 'Entrepreneurship'],
+        description: 'A blog focused on high-profile people leading tech companies around the world. As a writer and editor, I made an effort to show a diverse entrepreneur ecosystem with an equal representation of gender and ethnicities.',
+        sample: 'https://executives.technology/kim-beom-su-en/',
+    },
+    {
+        title: 'Developerz.software',
+        image: '',
+        url: 'https://developerz.software',
+        language: ['English', 'Spanish'],
+        topic: ['Start-ups', 'Software Development', 'Technology'],
+        description: 'A blog focused on topics related to software development, from business ideas to tools descriptions. As a writer, I expanded the subjects of the blog writing about most specialized issues.',
+        sample: 'https://developerz.software/2019/09/17/internships-spark-great-careers-case-study-blue-trail-software/',
+    },
+    {
+        title: 'Arrivedo',
+        image: '',
+        url: 'https://arrivedo.com',
+        language: ['English'],
+        topic: ['Travel guides'],
+        description: 'A website of tourist guides. As a writer, I interviewed resort owners and implemented best SEO practices to improve the impact of the content.',
+        sample: 'https://arrivedo.com/dest/en/guanajuato/hotel/sin-timon',
+    },
+];
